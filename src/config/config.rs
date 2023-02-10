9e3242558b4047cf81012bb370b8bd5acbe92676
asutos <asutos0714@outlook.com>
2023-02-10 11:14:48 +0800

    more features

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,6 +1,12 @@
-
-pub struct Config ();
-
-impl Config {
-    
+pub struct Config {
+    names: Vec<String>,
+    version_maps: Map<String, String>,
+    versions: Vec<Version>,
 }
+
+struct Version {
+    name: String,
+    path: String,
+}
+
+impl Config {}
