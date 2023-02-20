8d4a97737eb78c49edf28a3b5662b8e28e62bf60
asutos <asutos0714@outlook.com>
2023-02-20 14:24:01 +0800

    nil

--- tests/inte_test.rs
+++ tests/inte_test.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use directories::BaseDirs;
 use directories::UserDirs;
 
@@ -14,12 +16,42 @@ fn test_directories_base() {
         println!("data {:?}", base_dirs.data_dir());
         println!("preference_dir {:?}", base_dirs.preference_dir());
     }
-
 }
 
 #[test]
 fn test_directories_user() {
     if let Some(user_dirs) = UserDirs::new() {
-        println!("home : {:?}",user_dirs.home_dir());
+        println!("home : {:?}", user_dirs.home_dir());
     }
 }
+
+#[test]
+fn test_nil_2() {
+    let mut map = HashMap::<String, String>::new();
+
+    let key = "key".to_string();
+
+    match map.get_mut(&key) {
+        Some(value) => Some(value.to_owned()),
+        None => map.insert(key, "".to_string()),
+    };
+}
+
+#[test]
+fn test_nil_1() {
+    let mut data = vec!['a', 'b', 'c', 'd'];
+    let slice = &mut data[..];
+
+    p(slice);
+
+    data.push('d');
+    data.push('e');
+    data.push('f');
+}
+
+fn p(a: &mut [char]) {
+    println!("{:?}", a);
+}
+
+
+
